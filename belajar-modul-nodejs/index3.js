a354ca5543fd81dff9e24c53244e9b74aba0615b
andika-yudhistira <andikayudhistira.it@gmail.com>
2021-10-07 10:19:46 +0700

    Fix index3.js

--- belajar-modul-nodejs/index3.js
+++ belajar-modul-nodejs/index3.js
@@ -4,9 +4,10 @@ var http = require('http')
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type' : 'text/html'})
     res.write(salam.salamSayang())
-    res.write("Saat ini:" + moment().format('D MMMM YYYY HH:mm:ss'))
+    res.write("<br><h1>Saat ini:</h1>" + moment().format('D MMMM YYYY HH:mm:ss'))
     res.end()
 }).listen(8000)
 
 console.log('server connected at port 8000')
+console.log(salam.salamSayang())
 console.log('Saat ini: ' + moment().format('D MMMM YYYY HH:mm:ss'))
